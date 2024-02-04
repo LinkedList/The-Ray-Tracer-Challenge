5a10fc0071aa2f0d8e6e99f3a7f95c469782f4a5
Martin Macko <macko.mar@gmail.com>
2024-02-04 11:55:57 +0100

    Canvas to ppm pixels + spec

--- canvas.lua
+++ canvas.lua
@@ -1,8 +1,9 @@
-local canvas_module = {}
+local module = {}
 local color_module = require("./colors")
 local color = color_module.color
+local floor = require('math').floor
 
-function canvas_module.canvas(width, height) 
+function module.canvas(width, height) 
   mt = {}          -- create the matrix
   for y=1,height do
     mt[y] = {}     -- create a new row
@@ -14,20 +15,51 @@ function canvas_module.canvas(width, height)
   return mt
 end
 
-function canvas_module.getWidth(canvas)
+function module.getWidth(canvas)
   return #canvas[1]
 end
 
-function canvas_module.getHeight(canvas)
+function module.getHeight(canvas)
   return #canvas
 end
 
-function canvas_module.pixel_at(canvas, x, y)
+function module.pixel_at(canvas, x, y)
   return canvas[y][x]
 end
 
-function canvas_module.write_pixel(canvas, x, y, color)
+function module.write_pixel(canvas, x, y, color)
   canvas[y][x] = color
 end
 
-return canvas_module
+function scale(num)
+  local n = num
+  if n > 1.0 then n = 1.0 end
+  if n < 0 then n = 0 end
+  return math.floor(n * 255 + 0.5) 
+end
+
+function module.canvas_to_ppm(canvas)
+  -- naive and stupid building for now
+  local width = module.getWidth(canvas)
+  local height = module.getHeight(canvas)
+  local buffer = 'P3\n'
+  buffer = buffer .. width .. ' ' .. height .. '\n'
+  buffer = buffer .. '255\n'
+
+  for y, line in pairs(canvas) do
+    for x, pixel in pairs(line) do
+      local red = scale(pixel.red)
+      local green = scale(pixel.green)
+      local blue = scale(pixel.blue)
+      buffer = buffer .. red .. ' ' .. green .. ' ' .. blue
+      
+      if x ~= width then buffer = buffer .. ' ' end
+    end
+
+    buffer = buffer .. '\n'
+  end
+
+  return buffer
+end
+
+return module
--- specs/canvas_spec.lua
+++ specs/canvas_spec.lua
@@ -4,11 +4,20 @@ local getWidth = canvas_module.getWidth
 local getHeight = canvas_module.getHeight
 local write_pixel = canvas_module.write_pixel
 local pixel_at = canvas_module.pixel_at
+local canvas_to_ppm = canvas_module.canvas_to_ppm
 
 local color_module = require("./colors")
 local color = color_module.color
 
 local inspect = require('inspect')
+
+local function lines(str) 
+  local result = {} 
+  for line in str:gmatch '[^\n]+' do 
+    table.insert(result, line) 
+  end 
+  return result 
+end 
 
 describe("Creating a canvas", function()
   it("Creates a canvas", function()
@@ -33,5 +42,35 @@ describe("Creating a canvas", function()
 
     assert.same(pixel_at(c, 2, 3), red)
 
+  end)
+
+  it('Constructing the PPM header', function()
+    local c = canvas(5, 3)
+
+    local ppm = canvas_to_ppm(c)
+    local ppm_lines = lines(ppm)
+    assert.same(ppm_lines[1], 'P3')
+    assert.same(ppm_lines[2], '5 3')
+    assert.same(ppm_lines[3], '255')
+  end)
+
+  it('Constructing the PPM pixel data', function()
+    local c = canvas(5, 3)
+    local c1 = color(1.5, 0, 0)
+    local c2 = color(0, 0.5, 0)
+    local c3 = color(-0.5, 0, 1)
+
+    write_pixel(c, 1, 1, c1)
+    write_pixel(c, 3, 2, c2)
+    write_pixel(c, 5, 3, c3)
+
+    local ppm = canvas_to_ppm(c)
+    local ppm_lines = lines(ppm)
+
+    print(inspect(ppm_lines))
+
+    assert.same(ppm_lines[4], '255 0 0 0 0 0 0 0 0 0 0 0 0 0 0')
+    assert.same(ppm_lines[5], '0 0 0 0 0 0 0 128 0 0 0 0 0 0 0')
+    assert.same(ppm_lines[6], '0 0 0 0 0 0 0 0 0 0 0 0 0 0 255')
   end)
 end)
