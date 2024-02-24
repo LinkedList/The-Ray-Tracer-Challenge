74b588b8f3bf000cb972334dff639bddf8984e25
Martin Macko <macko.mar@gmail.com>
2024-02-24 11:16:05 +0100

    Matrix of size 2x2

--- specs/matrices_spec.lua
+++ specs/matrices_spec.lua
@@ -17,5 +17,22 @@ describe("Constructing and inspecting a matrix", function()
 
     assert.num_eq(m[1][1], 1)
     assert.num_eq(m[1][4], 4)
+    assert.num_eq(m[2][1], 5.5)
+    assert.num_eq(m[2][3], 7.5)
+    assert.num_eq(m[3][3], 11)
+    assert.num_eq(m[4][1], 13.5)
+    assert.num_eq(m[4][3], 15.5)
+  end)
+
+  it("we should be able to construct a 2x2 matrix", function()
+    local m = {
+      {-3, 5},
+      {1 , -2}
+    }
+
+    assert.num_eq(m[1][1], -3)
+    assert.num_eq(m[1][2], 5)
+    assert.num_eq(m[2][1], 1)
+    assert.num_eq(m[2][2], -2)
   end)
 end)
